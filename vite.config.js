66980f6aa08fcabc59f2b3c24bb116118b7a9b25
varshitha101 <varshitha.k@tantragyaansolutions.com>
2025-04-24 18:30:29 +0530

    deploy test 1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
+
+  base: "/Custom_Query/",
   server: {
     host: "0.0.0.0",
     port: 5173,
